01cc4d6e94674b23f19759fffcf24cd4c02eb24e
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 19:33:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,17 +7,25 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/un\w+\ing)
+ text.scan(/un\w+\ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/).to_a
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) && text.match(/[\.!\?]\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\(?[0-9]{3}\)?(\s?|-?)[0-9]{3}(\s?|-?)[0-9]{4}(\s?|-?)\b/)
+    true
+  else
+    false
+  end
 end
